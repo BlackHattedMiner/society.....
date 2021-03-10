d486c7ea292bf4592e8779187a3513b3409a1670
Rohan Singh <73023108+BlackHattedMiner@users.noreply.github.com>
2021-03-10 17:11:25 +0530

    Update society.py

--- Name-Fact-Generator-master/society.py
+++ Name-Fact-Generator-master/society.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 while True :
     name = st.text_input("Enter Your Name", "Name" )
-    gender_input = st.text_input("Enter Your Gender (F for female, M for male)",  )
+    gender_input = st.text_input("Enter Your Gender (F for female, M for male)", "Gender" )
     if gender_input == "f" or gender_input == "F": heshe = "She"; hisher = "her"; guygirl = "girl"; himher = "her"; girlguy = "guy";break
     elif gender_input == "m" or gender_input == "M" : heshe = "He"; hisher = "His"; guygirl = "guy"; himher = "him"; girlguy = "girl";break
     else : st.text("Incorrect Gender Entered.")
